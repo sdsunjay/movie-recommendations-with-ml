7cae3c7485c5b9fed991954cd8e1ecca868dcd66
Sunjay <sdsunjay73@yahoo.com>
2018-09-19 04:00:31 -0400

    WIP - add_friends and add_movies

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   super.tap do |user|
     if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
       user.email = data["email"] if user.email.blank?
+      user.friends = add_friends()
     end
   end
 end
@@ -23,47 +24,83 @@ end
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
-      user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
-      user.gender = auth.extra.raw_info.gender # assuming the user model has a gender
+      user.name = auth.info.name   # assuming the user has a name
+      user.image = auth.info.image # assuming the user has an image
+      user.gender = auth.extra.raw_info.gender # assuming the user has a gender
+      unless auth.extra.raw_info.location.nil?
+        user.location = auth.extra.raw_info.location['name'] # assuming the user has a location
+      end
+      unless auth.extra.raw_info.hometown.nil?
+        user.hometown = auth.extra.raw_info.hometown['name'] # assuming the user has a hometown
+      end
       user.token = auth.credentials.token
-    # TODO fix this
-    # user.add_friends
       user.save
+      add_friends
+      add_movies
       user
     end
   end
 
-  def self.add_friends
-    @facebook.get_connection("me", "friends").each do |hash|
-      self.friendships.where(:name => hash['name'], :uid => hash['id']).first_or_create
+  private
+  # @movies = Facebook.get_object(current_user.token, '/me/movies?fields=name,picture,studio')
+  def self.add_movies()
+    @graph = Koala::Facebook::API.new(self.token)
+    # movies = @graph.get_object("me", "movies?fields=name")
+    movies = @graph.get_object("me?fields=movies")
+    # puts movies
+    unless movies.nil?
+      movies.each_with_index do |hash, index| %>
+        @movies = Movie.where(title: hash[index]['name'])
+        @movies.each do |movie|
+          Review.where(user_id: self.id, movie_id: movie.id).first_or_create
+        end
+      end
     end
   end
 
-  private
+  def self.add_friends()
+    @facebook ||= Koala::Facebook::API.new(self.token)
+    friends = @facebook.get_connection("me", "friends")
+    puts friends
+    unless friends.nil?
+      friends.each_with_index do |hash, index| %>
+        @users = User.where(uid: hash[index]['id'])
+        @users.each do |friend|
+          Friendship.where(user_id: self.id, friend_id: friend.id).first_or_create
+        end
+      end
+    end
+  end
+
+  def old_friends(token)
+    @facebook ||= Koala::Facebook::API.new(token)
+    #facebook.get_object("me?fields=name,picture")
+    # TODO only store Facebook IDs, not other information
+    friends = @facebook.get_connections("me", "friends")
+    return friends
+  end
+  # def friends
+  #  @friends = facebook { |fb| fb.get_connections("me", "friends") }
+  #  user_friends = User.where(uid: @friends.map { |f| f['id'] })
+  #  puts user_friends
+  #end
 
   #def facebook
   #  @facebook ||= Koala::Facebook::API.new(token)
   #end
 
 
-  def facebook()
-     @facebook ||= Koala::Facebook::API.new(token)
-      block_given? ? yield(@facebook) : @facebook
+  # def facebook()
+    # @facebook ||= Koala::Facebook::API.new(token)
+    # block_given? ? yield(@facebook) : @facebook
     # rescue Koala::Facebook::APIError =>
-      # logger.info e.to_s
+    # logger.info e.to_s
     #  nil
     # TODO only store Facebook IDs, not other information
     # friends = facebook.get_connections("me", "friends")
     #facebook.get_object("me?fields=movies")
     # get_object("me") {|data| data['education']}  # => only education section of profile
     # return friends
-  end
-
-  # def friends
-  #  @friends = facebook { |fb| fb.get_connections("me", "friends") }
-  #  user_friends = User.where(uid: @friends.map { |f| f['id'] })
-  #  puts user_friends
-  #end
+  # end
 
 end
