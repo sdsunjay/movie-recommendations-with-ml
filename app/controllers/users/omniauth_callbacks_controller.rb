4010a7b69141407fee4971aa0fa8804a45378c27
Sunjay <sdsunjay73@yahoo.com>
2018-09-21 01:58:49 -0400

    Added intent

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,20 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])
 
+  def facebook
+    # TODO fix this
+    intent = request.env["omniauth.auth"]["intent"]
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    # @user = User.from_omniauth(auth, current_user)
+    #puts auth
     if @user.persisted?
+      #if intent == "sign_in"
+        #  puts intent
+        # Don't create a new identity
+      if intent == "sign_up"
+        # sign up flow, such as:
+        User.add_friends(@user.token, @user.id)
+        User.add_movies(@user.token, @user.id)
+      end
       sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
-      # @user.update token: request.env['omniauth.auth'].credentials.token
-      # @user.update friends: User.add_friends(@user.token)
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
+      redirect_to unauthenticated_root
     end
   end
 
   def failure
-    redirect_to root_path
+    redirect_to unauthenticated_root
     set_flash_message(:notice, :error, :kind => "Facebook") if is_navigational_format?
   end
 
