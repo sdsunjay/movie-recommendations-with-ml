4b2609457886acccc61927475b568f9e38111cb2
Sunjay <sdsunjay73@yahoo.com>
2018-09-21 00:10:40 -0400

    Validate uniqueness of user_id and friend_id

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,5 +2,5 @@ class Friendship < ApplicationRecord
   belongs_to :user
 	belongs_to :friend, class_name: "User"
   validates :user_id, presence: true
-  validates :friend_id, presence: true
+  validates :friend_id, uniqueness: { scope: :user_id, message: "You're already friends!" }
 end
