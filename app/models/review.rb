a5b9e3fd5ce132a14ec85269eb3bf92376cefade
Sunjay <sdsunjay73@yahoo.com>
2018-09-26 19:03:09 -0400

    Update comment

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,13 +2,13 @@ class Review < ApplicationRecord
 
   # user will review many movies
 	belongs_to :user
-	# post will have many reviews
+	# movie will have many reviews
 	belongs_to :movie
 	validates :user_id, presence: { message: 'Your review must belong to a user'}
 	validates :movie_id, presence: { message: 'Your review must belong to a movie'}
   validates :rating, presence: { message: 'You must give the movie a rating'}
 
-	# the driver (owner of the post) can review multiple passengers, but not the same passenger in the same post
+	# the user cannot review the same movie twice
   validates :movie_id, uniqueness: { scope: :user_id, message: "You've already reviewed this movie!" }
   def has_review?
     return if Review.exists?(user: user, movie_id: movie_id)
