aad22e23c7a9dbb9dc99a823941be68c5fa6d9c9
Sunjay <sdsunjay73@yahoo.com>
2018-09-26 19:04:57 -0400

    removed jquery3 and rails-ujs, replaced former with juery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,11 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery3
+//= require jquery
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
-//= require jquery_ujs
-//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require cocoon
