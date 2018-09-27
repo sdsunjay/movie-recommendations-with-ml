9ac22d5c8f32e18cf4b6dfc5e511e687ff7a0200
Sunjay <sdsunjay73@yahoo.com>
2018-09-27 00:50:32 -0400

    replace unauthenticated_root with unauthenticated_root_path

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -20,7 +20,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to unauthenticated_root
+      redirect_to unauthenticated_root_path
     end
   end
 
