d006fb50c4083b8eb93dc2725f08804f82aa9c29
Sunjay <sdsunjay73@yahoo.com>
2018-09-27 00:54:27 -0400

    token_expires_at added, get long lived token, auto update token on expiration, pagination of user friends and movies completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable
   # :registerable
   devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :omniauthable, omniauth_providers: [:facebook]
-
+  # has_many :friendships, class_name:  "Friendship",
+  #   foreign_key: "friend_id",
+  #  dependent:   :destroy
   has_many :reviews, dependent: :destroy
   has_many :movies, dependent: :destroy
   has_many :friendships, dependent: :destroy
@@ -11,18 +13,28 @@ class User < ApplicationRecord
 
   enum access_level: [:user, :admin, :super_admin]
 
-  # TODO - this may need to be moved
   def self.new_with_session(params, session)
     super.tap do |user|
       if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
         user.email = data["email"] if user.email.blank?
-        user.friends = add_friends
+        # user.friends = add_friends
       end
     end
   end
 
   def self.from_omniauth(auth)
+
+     # immediately get 60 day auth token
+    oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
+    new_access_info = oauth.exchange_access_token_info auth.credentials.token
+
+    new_access_token = new_access_info["access_token"]
+    # Facebook updated expired attribute
+    new_access_expires_at = DateTime.now + new_access_info["expires_in"].to_i.seconds
+
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user has a name
@@ -34,64 +46,87 @@ class User < ApplicationRecord
       unless auth.extra.raw_info.hometown.nil?
         user.hometown = auth.extra.raw_info.hometown['name'] # assuming the user has a hometown
       end
-      user.token = auth.credentials.token
-      # TODO - add this
-      # user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.token = new_access_token # originally auth.credentials.token
+      user.token_expires_at = new_access_expires_at # originally Time.at(auth.credentials.expires_at)
       user.save
       user
     end
-    # add_friends(auth.credentials.token)
-    # add_movies(auth.credentials.token)
   end
 
   # fbgraph(token).get_object(id, args, options, &block)
   # @movies = Facebook.get_object(current_user.token, '/me/movies?fields=name,picture,studio')
   # Facebook.get_object(current_user.token, '/me/books?fields=name,picture,written_by')
 
-  def self.add_movies
-    @graph = Koala::Facebook::API.new(self.token)
-    # movies = @graph.get_object("me", "movies?fields=name")
-    # @movies = @graph.get_object("/me/movies?fields=name")
-    @movies = @graph.get_object("me/movies/", {}, api_version: "v3.1")
-    # TODO - add pagination
-    puts 'Movies: '
-    puts @movies
-    unless @movies.nil?
-      @movies.each do |hash|
-        # puts hash
-        @user_movie = Movie.where("title = ?", hash["name"])
-        @user_movie.each do |movie|
-          Review.create(movie_id: movie.id, user_id: self.id, rating: 5)
+  def add_movies
+    @facebook ||= client()
+    @movies = @facebook.get_object("me/movies/", {}, api_version: "v3.1")
+    loop do
+      if @movies.present?
+        # TODO do where for all movies on page, (less queries = faster)
+        @movies.each do |hash|
+          @user_movie = Movie.where("title = ?", hash["name"])
+          @user_movie.each do |movie|
+            Review.create(movie_id: movie.id, user_id: id, rating: 5)
+          end
         end
-        # Review.where(user: user_id, movie_id: @movie.id).first_or_create
+      else
+        break
       end
+      @movies = @movies.next_page
+
     end
   end
 
-  def self.add_friends
-    @facebook ||= Koala::Facebook::API.new(self.token)
-    @friends = @facebook.get_connection("me", "friends")
-    # TODO - add pagination
-    puts @friends
-    unless @friends.nil?
-      # @user_friends= User.where(["uid = '%s'", @friends.to_s])
-      # @user_friends =  User.where(["uid = :id", @friends])
-      # @user_friends.each do |friend|
-      #  puts @friend.id
-      #  User.friendships.create(user_id: user_id, friends_id: @friend.id)
-      #end
-      # puts 'USERS: '
-      # puts @users
-      @friends.each do |hash|
-        # {"name"=>"Sunjay Dhama", "id"=>"10205306719984646"}
-        @user_friends = User.where(["uid = '%s'", hash["id"].to_s])
-        @user_friends.each do |friend|
-          Friendship.create(friend_id: friend.id, user_id: self.user_id)
+  def add_friends
+    @facebook ||= client
+    @friends = @facebook.get_connections("me", "friends")
+    loop do
+      if @friends.present?
+        # TODO do where for all friends on page, (less queries = faster)
+        @friends.each do |hash|
+          # {"name"=>"Sunjay Dhama", "id"=>"10205306719984646"}
+          @user_friends = User.where(["uid = '%s'", hash["id"].to_s])
+          @user_friends.each do |friend|
+            Friendship.create(friend_id: friend.id, user_id: id)
+          end
         end
+      else
+        break
       end
+      @friends = @friends.next_page
     end
+
   end
 
+  def client
+    # send("#{provider}_client")
+    facebook_client
+  end
+
+  def expired?
+      token_expires_at? && token_expires_at <= Time.zone.now
+  end
+
+   def facebook_client
+    Koala::Facebook::API.new(access_token)
+  end
+
+  def access_token
+    # send("#{provider}_refresh_token!", super) if expired?
+    facebook_refresh_token if expired?
+    token
+  end
+
+  def facebook_refresh_token
+    # Get refreshed 60 day auth token
+    oauth = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_SECRET"])
+    new_access_info = oauth.exchange_access_token_info token
+    update(token: new_access_info["access_token"], token_expires_at: DateTime.now + new_access_info["expires_in"].to_i.seconds)
+  end
+
+  def put_wall_post(message)
+    @graph.put_wall_post(message)
+  end
   private
 
   def old_friends(token)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_12_002202) do
+ActiveRecord::Schema.define(version: 2018_09_27_023250) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -97,6 +97,7 @@ ActiveRecord::Schema.define(version: 2018_09_12_002202) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "token_expires_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
