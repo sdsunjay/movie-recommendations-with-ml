9d18457ae0ac7cfe11ee97e29f8e6b1b67d1e495
Sunjay <sdsunjay73@yahoo.com>
2018-09-29 03:24:31 -0400

    WIP

---
+++ app/controllers/facebook_realtime_updates_controller.rb
@@ -0,0 +1,22 @@
+class FacebookRealtimeUpdatesController < ApplicationController
+
+  def verify
+    challenge = Koala::Facebook::RealtimeUpdates.meet_challenge(params, FACEBOOK_REALTIME_UPDATE_VERIFY_TOKEN)
+    respond_to do |format|
+      format.text do
+        render text: challenge
+      end
+    end
+  end
+
+  def update
+    # TODO: Handle an incoming POST from Facebook
+    head :ok
+  end
+
+  def subscribe
+    updates = Koala::Facebook::RealtimeUpdates.new(:app_id => FACEBOOK_APP_ID, :secret => FACEBOOK_APP_SECRET)
+    updates.subscribe("user", "friends,movies", FACEBOOK_REALTIME_UPDATE_CALLBACK_URL, FACEBOOK_REALTIME_UPDATE_VERIFY_TOKEN)
+    head :ok
+  end
+end
