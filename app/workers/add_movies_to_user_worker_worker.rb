bb03c3bb375386b6621464b6e398c2e8eca4b41f
Sunjay <sdsunjay73@yahoo.com>
2018-10-01 14:35:19 -0400

    Removed debugging statement

--- app/workers/add_movies_to_user_worker_worker.rb
+++ app/workers/add_movies_to_user_worker_worker.rb
@@ -3,7 +3,6 @@ class AddMoviesToUserWorker
   sidekiq_options queue: 'critical', :retry => 5, :dead => false # will retry 5 times and then disappear
 
   def perform(user_id)
-    puts 'IN ADD MOVIES TO USER WORKER'
     @user = User.find(user_id)
     if @user
       @user.add_movies
