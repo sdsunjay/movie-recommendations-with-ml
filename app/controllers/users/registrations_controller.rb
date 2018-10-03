dc179dc56883bb9f4c434c5b8af0869cfccc8ce2
Sunjay <sdsunjay73@yahoo.com>
2018-10-02 21:06:40 -0400

    Removed AddMoviesToUserWorker and AddFriendsToUserWorker because 'after_sign_up_path_for' is not called when a user registers with Facebook

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,10 @@
 # users/registrations_controller.rb
 class Users::RegistrationsController < Devise::RegistrationsController
-
   def after_sign_up_path_for(resource)
-    puts "IN AFTER SIGN UP FOR"
-    AddMoviesToUserWorker.perform_async(current_user.id)
-    AddFriendsToUserWorker.perform_async(current_user.id)
     signed_in_root_path(resource)
   end
 
   def after_update_path_for(resource)
     signed_in_root_path(resource)
   end
-
 end
