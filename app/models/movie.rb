a405bf5a40371be487348d4dbf4c0c83965b26c6
Sunjay <sdsunjay73@yahoo.com>
2018-10-07 18:53:25 -0400

    Updated search as @lifeiscontent suggested

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,16 +1,19 @@
- # model/movie.rb
+# frozen_string_literal: true
+
+# a top level comment
 class Movie < ApplicationRecord
   belongs_to :user
   has_many :review, dependent: :delete_all
   has_many :categorizations, dependent: :delete_all
-  has_many :genres, through: :categorizations
+  has_many :genres, ->{ select(:name, :id) }, through: :categorizations
 
   validates :title, presence: true
   validates :overview, presence: true
   validates :poster_path, presence: true
   validates :release_date, presence: true
 
-  accepts_nested_attributes_for :categorizations, reject_if: :all_blank, allow_destroy: true
+  accepts_nested_attributes_for :categorizations,
+                                reject_if: :all_blank, allow_destroy: true
   accepts_nested_attributes_for :genres
 
   def self.search(pattern)
@@ -18,12 +21,11 @@ class Movie < ApplicationRecord
     if pattern.blank?
       all
     else
-      where('lower(title) LIKE lower(?)', "%#{pattern}%")
+      where(arel_table[:title].lower.matches("%#{pattern}%".downcase))
     end
   end
 
   def movie_review(user_id, movie_id)
     Review.where(user: user_id, movie_id: movie_id).first
   end
-
 end
