37fe5cc601dc7b890de73aaf7f25967dfd458a9b
Sunjay <sdsunjay73@yahoo.com>
2018-10-11 20:22:20 -0400

    Added some config

--- config/puma.rb
+++ config/puma.rb
@@ -21,14 +21,33 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
 # process behavior so workers use less memory.
 #
-# preload_app!
+preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+
+# Because we are using preload_app, an instance of our app is created by master process (calling our initializers) and then memory space
+# is forked. So we should close DB connection in the master process to avoid connection leaks.
+# https://github.com/puma/puma/issues/303
+# http://stackoverflow.com/questions/17903689/puma-cluster-configuration-on-heroku
+# http://www.rubydoc.info/gems/puma/2.14.0/Puma%2FDSL%3Abefore_fork
+# Dont have to worry about Sidekiq's connection to Redis because connections are only created when needed. As long as we are not
+# queuing workers when rails is booting, there will be no redis connections to disconnect, so it should be fine.
+before_fork do
+  puts "Puma master process about to fork. Closing existing Active record connections."
+  ActiveRecord::Base.connection.disconnect!
+end
+
+on_worker_boot do
+  # Worker specific setup for Rails 4.1+
+  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+  ActiveRecord::Base.establish_connection
+end
