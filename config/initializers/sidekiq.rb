46613785dd14f287999984ab298f3f4209286d05
Sunjay <sdsunjay73@yahoo.com>
2018-10-11 20:21:26 -0400

    Added stuff for production environment

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,45 @@
-REDIS_URL = Rails.application.credentials.development[:redis_url]
-REDIS_PASSWORD = Rails.application.credentials.development[:redis_password]
-Sidekiq.configure_server do |config|
-  config.redis = { url: REDIS_URL, password: REDIS_PASSWORD }
+if Rails.env.development?
+
+
+  REDIS_URL = Rails.application.credentials.development[:redis_url]
+  REDIS_PASSWORD = Rails.application.credentials.development[:redis_password]
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: REDIS_URL, password: REDIS_PASSWORD, size: 2}
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { url: REDIS_URL, password: REDIS_PASSWORD, size: 20 }
+  end
+
 end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: REDIS_URL, password: REDIS_PASSWORD}
+if Rails.env.production?
+  REDIS_URL = Rails.application.credentials.production[:redis_url]
+  REDIS_PASSWORD = Rails.application.credentials.production[:redis_password]
+
+  Sidekiq.configure_client do |config|
+    config.redis = { url: REDIS_URL, password: REDIS_PASSWORD, size: 2}
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: REDIS_URL, password: REDIS_PASSWORD, size: 20 }
+
+    Rails.application.config.after_initialize do
+      Rails.logger.info("DB Connection Pool size for Sidekiq Server before disconnect is: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+      ActiveRecord::Base.connection_pool.disconnect!
+
+      ActiveSupport.on_load(:active_record) do
+        config = Rails.application.config.database_configuration[Rails.env]
+        config['reaping_frequency'] = ENV['DATABASE_REAP_FREQ'] || 10 # seconds
+        # config['pool'] = ENV['WORKER_DB_POOL_SIZE'] || Sidekiq.options[:concurrency]
+        config['pool'] = 16
+        ActiveRecord::Base.establish_connection(config)
+
+        Rails.logger.info("DB Connection Pool size for Sidekiq Server is now: #{ActiveRecord::Base.connection.pool.instance_variable_get('@size')}")
+      end
+    end
+  end
+
 end
+
+
