5fc605c51bef0ec292995e7b09ccc53d2bfd8c82
Sunjay <sdsunjay73@yahoo.com>
2018-11-19 20:11:34 -0500

    remove friends column from Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_12_034544) do
+ActiveRecord::Schema.define(version: 2018_11_20_010401) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -217,7 +217,6 @@ ActiveRecord::Schema.define(version: 2018_11_12_034544) do
     t.text "image"
     t.string "link"
     t.integer "access_level"
-    t.text "friends"
     t.string "education"
     t.string "provider"
     t.string "uid"
