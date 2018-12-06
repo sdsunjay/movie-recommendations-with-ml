54fd8c5751357a1fa2747813be141e6ba3ac2d35
Sunjay <sdsunjay73@yahoo.com>
2018-12-05 22:02:45 -0500

    Removed position and page_number from movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_010401) do
+ActiveRecord::Schema.define(version: 2018_12_06_022425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -191,8 +191,6 @@ ActiveRecord::Schema.define(version: 2018_11_20_010401) do
     t.integer "runtime"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "position", default: 0, null: false
-    t.integer "page_number", limit: 2, default: 0, null: false
     t.index ["title"], name: "index_movies_on_title"
   end
 
