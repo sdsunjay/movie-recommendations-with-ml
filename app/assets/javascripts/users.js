59a0dd7861052871bd544a906c68d1b48f69e452
Sunjay <sdsunjay73@yahoo.com>
2018-12-06 17:15:29 -0500

    autocomplete user education field with university name (WIP)

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,7 @@
 window.addEventListener("turbolinks:load", Pagy.init);
+jQuery(function() {
+  return $('#user_education').autocomplete({
+    source: ['Cal Poly', 'UC Berkeley', 'UC Davis']
+    //source: $('#product_category_name').data('autocomplete-source')
+  });
+});
