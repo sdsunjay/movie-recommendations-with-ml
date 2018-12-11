b4743337e612b256110056810b9f6ccf3ca355bf
Sunjay <sdsunjay73@yahoo.com>
2018-12-10 23:15:16 -0500

    remove unnecessary stuff

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,1 @@
 window.addEventListener("turbolinks:load", Pagy.init);
-jQuery(function() {
-  return $('#user_education').autocomplete({
-    source: ['Cal Poly', 'UC Berkeley', 'UC Davis']
-    //source: $('#product_category_name').data('autocomplete-source')
-  });
-});
