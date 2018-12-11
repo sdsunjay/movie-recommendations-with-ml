2b950ab52bd6b9dda30e847cfbd8f91c70a061a7
Sunjay <sdsunjay73@yahoo.com>
2018-12-11 18:53:53 -0500

    Moved code into users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,1 +1,17 @@
 window.addEventListener("turbolinks:load", Pagy.init);
+window.addEventListener("turbolinks:load", function() {
+  var $input = $("[data-behavior='autocomplete_education']");
+  var options = {
+    getValue: "name",
+    url: function(phrase) {
+      return "/autocomplete/education.json?name=" + phrase;
+    },
+    categories: [
+      {
+        listLocation: "educations",
+        header: "<strong>Universities</strong>"
+      }
+    ]
+  };
+  $input.easyAutocomplete(options);
+});
