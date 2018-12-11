72d0910340fc1ba9ab3cd8611fcb068af37edc6e
Sunjay <sdsunjay73@yahoo.com>
2018-12-10 21:55:02 -0500

    force_json for request.format

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,4 +81,8 @@ class ApplicationController < ActionController::Base
   def track_action
     ahoy.track 'Ran action', request.path_parameters
   end
+
+  def force_json
+    request.format = :json
+  end
 end
