0de86cc82a2c5d8c44006e26380c4562ee25319a
Sunjay <sdsunjay73@yahoo.com>
2018-12-10 21:56:34 -0500

    Added a lot of stuff to search_controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,31 @@
 # /app/controllers/search_controller.rb
 class SearchController < ApplicationController
-  def index
-    if params.keys.all? { |key, _value| ['title'].include? key }
-      @movies = Movie.search(params[:title]).order(created_at: :desc)
+  before_action :force_json, only: :autocomplete
+  def autocomplete
+    @movies = Movie.ransack(title_cont: params[:title]).result(distinct: true)
+    # @directors = Director.ransack(name_cont: params[:q]).result(distinct: true)
+
+    respond_to do |format|
+      format.json {
+        @movies = @movies.limit(30)
+        # @directors = @directors.limit(5)
+      }
     end
+
+    # @movies = Movie.order(popularity: :asc).where("title like ?", "%#{params[:title]}%").limit(100)
+  end
+
+  def search
+    @pagy, @movies = pagy(Movie.ransack(title_cont: params[:title]).result(distinct: true), items: 30)
+    # ahoy.track 'Searched movie', title: params[:title]
+    return if @movies.exists?
+
+    ahoy.track 'Movie not found'
+    flash[:alert] = movie_title + ' not found'
+    params.delete :title
+    redirect_back(fallback_location: movies_path)
+    # @directors = Director.ransack(name_cont: params[:q]).result(distinct: true)
+
+    # @movies = Movie.order(popularity: :asc).where("title like ?", "%#{params[:title]}%").limit(100)
   end
 end
