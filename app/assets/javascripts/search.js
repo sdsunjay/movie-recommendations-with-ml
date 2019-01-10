ec874d659b351f60a5a92c8a9439a29a523bd5e2
Sunjay <sdsunjay73@yahoo.com>
2019-01-10 18:21:35 -0500

    Add delay, set max number of element in list to 10, removed categories

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -2,16 +2,16 @@ window.addEventListener("turbolinks:load", function() {
   var $input = $("[data-behavior='autocomplete']");
   var options = {
     getValue: "name",
+    requestDelay: 350,
     url: function(phrase) {
       return "/autocomplete/search.json?title=" + phrase;
     },
-    categories: [
-      {
-        listLocation: "movies",
-        header: "<strong>Movies</strong>"
-      }
-    ],
+    listLocation: "movies",
     list: {
+      maxNumberOfElements: 10,
+      match: {
+        enabled: true
+      },
       onChooseEvent: function () {
         var url = $input.getSelectedItemData().url
         $input.val("")
