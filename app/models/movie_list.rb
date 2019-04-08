86feec55a04588b4764142732c42fddecbcf6ce7
Sunjay <sdsunjay73@yahoo.com>
2019-04-08 18:25:04 -0400

    movie lists feature

---
+++ app/models/movie_list.rb
@@ -0,0 +1,21 @@
+class MovieList < ApplicationRecord
+  default_scope { order(created_at: :desc) }
+  # list can have many  movies
+  belongs_to :list
+  # list can have many movies
+  belongs_to :movie, -> { order(created_at: :desc) }
+  validates :list_id,
+            presence: { message: 'Movie must belong to a list' }
+  validates :movie_id,
+            presence: { message: 'Specify Movie ID' }
+  # the user cannot review the same movie twice
+  validates :movie_id,
+            uniqueness: { scope: :list_id,
+                          message: "You've already added this movie!" }
+
+  # no more than 100 movies per list
+  def limit_exceeded?(max = 100)
+    self.lists.count >= max
+  end
+
+end
