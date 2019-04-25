7f3fc14efd98ff80d7bb966fd430c132cffa21f1
Sunjay <sdsunjay73@yahoo.com>
2019-04-25 00:08:34 -0400

    added comments

--- app/models/movie_list.rb
+++ app/models/movie_list.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# a top level comment
 class MovieList < ApplicationRecord
   default_scope { order(created_at: :desc) }
   # list can have many  movies
@@ -8,7 +11,7 @@ class MovieList < ApplicationRecord
             presence: { message: 'Movie must belong to a list' }
   validates :movie_id,
             presence: { message: 'Specify Movie ID' }
-  # the user cannot review the same movie twice
+  # the user cannot add the same movie to a list twice
   validates :movie_id,
             uniqueness: { scope: :list_id,
                           message: "You've already added this movie!" }
