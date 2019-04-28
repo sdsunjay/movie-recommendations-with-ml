82b7f6c20af37b76999d09b60c1834da5269d8f2
Sunjay <sdsunjay73@yahoo.com>
2019-04-28 19:20:58 -0400

    Added 'create_inverse_relationship' and 'destroy_inverse_relationship', added validation 'not_self'

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,13 +2,33 @@
 
 # a top level comment
 class Friendship < ApplicationRecord
-  belongs_to :user
-  belongs_to :friendship, class_name: 'User', foreign_key: 'friend_id'
+  after_create :create_inverse_relationship
+  after_destroy :destroy_inverse_relationship
 
+  belongs_to :user
+  belongs_to :friend, class_name: 'User', foreign_key: 'friend_id'
+
+  validate :not_self
   validates :user_id, presence: true,
                       uniqueness: { scope: :friend_id,
-                                    message: 'You cannot be friends with yourself!' }
+                                    message: :"You're already friends!" }
   validates :friend_id,
             uniqueness: { scope: :user_id,
                           message: "You're already friends!" }
+
+  private
+
+  def create_inverse_relationship
+    friend.friendships.create(friend: user)
+  end
+
+  def destroy_inverse_relationship
+    friendship = friend.friendships.find_by(friend: user)
+    friendship.destroy if friendship
+  end
+
+  def not_self
+    errors.add(:friend, "can't be equal to user") if user == friend
+  end
+
 end
