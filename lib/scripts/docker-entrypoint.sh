f909761e2da4e09c1a76a94cc080cf55abe6d16f
Sunjay Dhama <sdsunjay73@yahoo.com>
2020-07-28 08:40:07 +0000

    remove GeoIP

--- lib/scripts/docker-entrypoint.sh
+++ lib/scripts/docker-entrypoint.sh
@@ -13,6 +13,7 @@ fi
 # bundle exec rake db:reset DISABLE_DATABASE_ENVIRONMENT_CHECK=1
 
 bundle exec rake db:environment:set RAILS_ENV=production
+bundle exec rake db:migrate RAILS_ENV=production
 
 if [[ $? != 0 ]]; then
   echo
